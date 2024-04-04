9abd0ed7c51e0718250684ed235765673d032c5e
contactmizan <contactmizan1206@gmail.com>
2024-04-04 16:26:21 +0600

    added a loader from react-awesome components

--- src/component/Phones/Phones.jsx
+++ src/component/Phones/Phones.jsx
@@ -1,11 +1,13 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, Tooltip } from 'recharts';
+import { Audio } from 'react-loader-spinner'
 
 
 const Phones = () => {
 
     const [phones, setPhones] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         // fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
@@ -23,11 +25,22 @@ const Phones = () => {
                     return obj;
                 })
                 console.log(phonesWithFakeData);
-                setPhones(phonesWithFakeData)
+                setPhones(phonesWithFakeData);
+                setLoading(false);
             })
     }, [])
     return (
         <div>
+            {loading && <Audio
+                height="80"
+                width="80"
+                radius="9"
+                color="green"
+                ariaLabel="three-dots-loading"
+                wrapperStyle
+                wrapperClass
+            />}
+
             <h2 className="text-5xl">Phones: {phones.length}</h2>
 
             <BarChart width={1200} height={400} data={phones}>
